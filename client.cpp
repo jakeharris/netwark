cdd5535ba7a99c7c16c7d11d955acd3644edad7d
javakat <milieu@javakat.io>
2014-03-19 13:14:09 -0500

    Create basic server, allow sending messages to server

--- client.cpp
+++ client.cpp
@@ -11,14 +11,14 @@ int main() {
   
   int s = 0;
 
-  string hs = "131.204.254.100"; /* Needs to be updated? Might be a string like "tux175.engr.auburn.edu." */
+  string hs = "54.186.88.157"; /* Needs to be updated? Might be a string like "tux175.engr.auburn.edu." */
   short int port = 10038; /* Can be any port within 10038-10041, inclusive. */
 
   struct sockaddr_in a;
   struct sockaddr_in sa;
   struct hostent *h;
 
-
+  string m = "Hello, server world!";
 
   if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
     cout << "Socket creation failed. (socket s)" << endl;
@@ -38,15 +38,17 @@ int main() {
   memset((char *)&sa, 0, sizeof(sa));
   sa.sin_family = AF_INET;
   sa.sin_port = htons(port);
-  memcpy((void *)&sa.sin_addr, hs.c_str(), strlen(hs.c_str()));
+  inet_aton(hs.c_str(), &sa.sin_addr);
 
-  cout << "Host string: " << hs.c_str() << endl;
-  cout << "Length of host string: " << strlen(hs.c_str()) << endl;
-
-  cout << "Server address: " << sa.sin_addr.s_addr << endl;
+  cout << "Server address: " << inet_ntoa(sa.sin_addr) << endl;
   cout << "Port: " << ntohs(sa.sin_port) << endl;
   cout << "Client address: " << ntohl(a.sin_addr.s_addr) << endl;
   cout << "Port: " << ntohs(a.sin_port) << endl;
+
+  if(sendto(s, m.c_str(), strlen(m.c_str()), 0, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+    perror("Package sending failed. (socket s, server address sa, message m)");
+    return 0;
+  }
 
   return 0;
 }
---
+++ server.cpp
@@ -0,0 +1,46 @@
+#define PORT 1153
+#define BUFSIZE 2048
+
+int main() {
+  struct sockaddr_in a;
+  struct sockaddr_in ca;
+  socklen_t calen = sizeof(ca);
+  int rlen;
+  int s;
+  unsigned char b[BUFSIZE];
+
+  /* Create our socket. */
+  if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    cout << "Socket creation failed. (socket s)" << endl;
+    return 0;
+  }
+
+  /* 
+   * Bind our socket to an IP (whatever the computer decides) 
+   * and a specified port. 
+   *
+   */
+
+  memset((char *)&a, 0, sizeof(a));
+  a.sin_family = AF_INET;
+  a.sin_addr.s_addr = htonl(INADDR_ANY);
+  a.sin_port = htons(PORT);
+
+  if (bind(s, (struct sockaddr *)&a, sizeof(a)) < 0) {
+    cout << "Socket binding failed. (socket s, address a)" << endl;
+    return 0;
+  }
+
+  /* Loop forever, waiting for messages from a client. */
+
+  cout << "Waiting on port " << PORT << "..." << endl;
+
+  for (;;) {
+    rlen = recvfrom(s, b, BUFSIZE, -, (struct sockaddr *)&ca, &calen);
+    cout << "Received " << rlen << " bytes." << endl;
+    if (rlen > 0) {
+      buf[rlen] = 0;
+      cout << "Received message: " endl << buf << endl;
+    }
+  }
+}
